d748440fa2ea21b17d10d9390c54b21c40dd2e95
ujshandi@gmail.com <ujshandi@gmail.com@c52945b5-dd79-e41c-3719-88f061420335>
2014-08-11 08:00:42 +0000

    - modif config & htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-	$config['base_url'] = "http://".$_SERVER['HTTP_HOST']."/anev/";
+	$config['base_url'] = "http://".$_SERVER['HTTP_HOST']."/e-anev/";
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['password'] = 'admin';
+$db['default']['database'] = 'e_anev';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
